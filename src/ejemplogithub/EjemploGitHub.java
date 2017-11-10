d4a1a2cbe01ca42b86b6ef6e53ced66b076c90af
an3shito <33529502+an3shito@users.noreply.github.com>
2017-11-10 15:42:56 -0600

    Update EjemploGitHub.java

--- src/ejemplogithub/EjemploGitHub.java
+++ src/ejemplogithub/EjemploGitHub.java
@@ -11,12 +11,12 @@ public class EjemploGitHub {
    
     public static void main(String[] args) {
         
-        Scanner in = new Scanner(System.in);
+        
         JOptionPane.showMessageDialog(null, "Hola Mundo");
         String nombre = JOptionPane.showInputDialog(null, "Ingrese algun nombre:");
         System.out.println("Hola Mundo");
         
-        String entrada = in.nextLine();
+        
         
         System.out.println("el nombre es:"+nombre);
         
