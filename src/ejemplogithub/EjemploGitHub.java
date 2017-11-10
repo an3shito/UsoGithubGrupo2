c8f1d900b0023d033596510ae0b22f62732cb3d3
Java <Java@DESKTOP-8BHSCM6>
2017-11-10 14:33:07 -0600

    nuevo jOption Andres

---
+++ src/ejemplogithub/EjemploGitHub.java
@@ -0,0 +1,17 @@
+
+package ejemplogithub;
+
+// Ejemplo de Github Grupo 2 Java
+
+import javax.swing.JOptionPane;
+
+public class EjemploGitHub {
+
+   
+    public static void main(String[] args) {
+        JOptionPane.showMessageDialog(null, "Hola Mundo");
+        System.out.println("Hola Mundo");
+        
+    }
+    
+}
