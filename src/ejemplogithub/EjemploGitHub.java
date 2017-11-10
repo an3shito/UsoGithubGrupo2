66758b4c4221590f289fbdcab34db078e47bbb75
Java <Java@DESKTOP-8BHSCM6>
2017-11-10 15:32:10 -0600

    Rama2 editada por andres

--- src/ejemplogithub/EjemploGitHub.java
+++ src/ejemplogithub/EjemploGitHub.java
@@ -3,14 +3,22 @@ package ejemplogithub;
 
 // Ejemplo de Github Grupo 2 Java
 
+import java.util.Scanner;
 import javax.swing.JOptionPane;
 
 public class EjemploGitHub {
 
    
     public static void main(String[] args) {
+        
+        Scanner in = new Scanner(System.in);
         JOptionPane.showMessageDialog(null, "Hola Mundo");
+        String nombre = JOptionPane.showInputDialog(null, "Ingrese algun nombre:");
         System.out.println("Hola Mundo");
+        
+        String entrada = in.nextLine();
+        
+        System.out.println("el nombre es:"+nombre);
         
     }
     
